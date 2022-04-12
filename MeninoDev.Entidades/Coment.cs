24571c014da59052fb7b52f0c6a23b331939c75f
Leomar <leomar.sartor@funcionalcorp.com.br>
2022-04-12 14:28:01 -0300

    chore: inclusão de entidades básicas

---
+++ MeninoDev.Entidades/Coment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeninoDev.Entidades
+{
+    public class Coment
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ MeninoDev.Entidades/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeninoDev.Entidades
+{
+    public class Post
+    {
+        public long Id { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
