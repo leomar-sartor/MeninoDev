8b252b6481b37e855b7167843e1dcea27c65faca
Leomar <leomar.sartor@funcionalcorp.com.br>
2022-04-12 16:18:22 -0300

    feat: Inserção do postagem

--- MeninoDev/Controllers/PostController.cs
+++ MeninoDev/Controllers/PostController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeninoDev.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Dapper;
+using System;
 
 namespace MeninoDev.Controllers
 {
     public class PostController : Controller
     {
+        public IConfiguration _configuration { get; }
+        public string _connectionString { get; }
+
+        public PostController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("database");
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = "Dados Inválidos!";
+                return View();
+            }
+            
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sql = @"INSERT POST (Date, Title, Content) OUTPUT INSERTED.* VALUES (@Date, @Title, @Content)";
+                post.Date = DateTime.Today;
+
+                var retorno = db.QuerySingle<Post>(sql, new
+                {
+                    post.Date,
+                    post.Title,
+                    post.Content
+                });
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
