049a2c830157779c43bc0005a8f198624442631f
Leomar <leomar.sartor@funcionalcorp.com.br>
2022-04-12 14:08:07 -0300

    Estrutura Inicial

---
+++ MeninoDev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeninoDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
